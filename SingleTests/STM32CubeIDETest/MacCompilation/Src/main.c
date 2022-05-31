f6ff0a5283864732695c0e1c1d219b9f9d240598
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-05-31 18:53:14 +0200

    Test di CMake

--- SingleTests/STM32CubeIDETest/MacCompilation/Src/main.c
+++ SingleTests/STM32CubeIDETest/MacCompilation/Src/main.c
@@ -17,11 +17,13 @@
  */
 
 #include <stdint.h>
-#include <stdio.h>
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
 
 int main(void)
 {
     /* Loop forever */
-	printf("Hello");
 	for(;;);
 }
