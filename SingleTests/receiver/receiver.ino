dc92266c59cc2c926a77ddb7ac80eb705e0930e3
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-06-09 17:11:35 +0200

    Test radio

--- SingleTests/receiver/receiver.ino
+++ SingleTests/receiver/receiver.ino
@@ -1,6 +1,6 @@
 #include <string.h>
 
-#define IBUS_BUFFSIZE 32    // Max iBus packet size (2 byte header, 14 channels x 2 bytes, 2 byte checksum)
+#define IBUS_BUFFSIZE 22    // Max iBus packet size (2 byte header, 14 channels x 2 bytes, 2 byte checksum)
 #define IBUS_MAXCHANNELS 10 // My TX only has 10 channels, no point in polling the rest
 
 int ledPin = 9;
@@ -9,34 +9,39 @@ bool cominciamo = false;
 bool finitoLettura = false;
 static uint8_t ibusIndex = 0;
 static uint8_t ibus[IBUS_BUFFSIZE] = {0};
-static uint16_t rcValue[IBUS_MAXCHANNELS]={0};
-
+static uint16_t rcValue[IBUS_MAXCHANNELS] = {0};
+uint16_t data[IBUS_MAXCHANNELS];
 static boolean rxFrameDone;
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Ciao Giovanni");
+  for (int i = 0; i < IBUS_MAXCHANNELS; i++) {
+    if(i == 1 or i == 7){
+    Serial.print("Ch");
+    Serial.print(i);
+    Serial.print(" ");}
+  }
+  Serial.println();
 }
 
 void loop()
 {
   readRx();
-  //delay(100);
+  
+  //delay(3);
 }
 
 void readRx()
 {
-  rxFrameDone = false;
-
   if (Serial.available())
   {
-
     uint8_t val = Serial.read();
     if (val == 32) {
       forse = true;
       return;
     }
+    
     if (forse && val == 64) {
       cominciamo = true;
       return;
@@ -45,34 +50,27 @@ void readRx()
     if (cominciamo) {
       ibus[ibusIndex] = val;
       ibusIndex++;
+      
       if (ibusIndex == IBUS_BUFFSIZE)
         finitoLettura = true;
 
+      
+      
       if (finitoLettura) {
+        //erial.println(ibus[10]);
         // Salvo
-        int high=1;
-        int low=0;
-        for(int i = 6; i < IBUS_BUFFSIZE; i++){
-          Serial.print(ibus[i]);
-          Serial.print(" ");
-        }
-
-        Serial.println();
-        
-        for (int i = 0; i < IBUS_MAXCHANNELS && false; i++) {
-          //left shift away the first 8 bits of the first byte and add the whole value of the previous one
-          rcValue[i] = ((ibus[high] << 8) & (0x0FFF)) + ibus[low];
-          
+        int high = 1;
+        int low = 0;
+      
+        for (int i = 0; i < IBUS_MAXCHANNELS; i++) {
+          rcValue[i] = (ibus[high] << 8) + ibus[low];
+          if(i == 1 or i == 7){
+          Serial.print(rcValue[i]);
+          Serial.print(" ");}
           high += 2;
           low += 2;
-          
-          
-          if(i == 1 && false){
-          Serial.print("Ch");
-          Serial.print(i+1);
-          Serial.print(" ");
-          Serial.println(rcValue[i]);}
         }
+        Serial.println();
 
         forse = false;
         cominciamo = false;
@@ -80,4 +78,5 @@ void readRx()
         ibusIndex = 0;
       }
     }
-}}
+  }
+}
