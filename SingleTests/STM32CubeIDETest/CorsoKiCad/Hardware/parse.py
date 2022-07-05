88ea1f41b760c5c95b2523068df573e4a7632b8e
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-07-05 20:18:42 +0200

    BOM

--- SingleTests/STM32CubeIDETest/CorsoKiCad/Hardware/parse.py
+++ SingleTests/STM32CubeIDETest/CorsoKiCad/Hardware/parse.py
@@ -3,5 +3,20 @@ tree = ET.parse('./HWCorso/HWCorso.xml')
 root = tree.getroot()
 components = root[1]
 
+print("Designator, Comment, Footprint, LCSC")
+
 for c in components:
-    print(c.attrib)
+    # Designator
+    print('"' + c.attrib["ref"] + '", ', end="")
+    # Comment
+    comment = c.find("value").text
+    print('"' + comment + '", ', end="")
+    # Footprint
+    footp = c.find("footprint").text  
+    print('"' + footp + '", ', end="")
+    # LCSC
+    lcsc = c.find("property").attrib["value"]
+    print('"' + lcsc + '"', end="")   
+
+    # end
+    print()
