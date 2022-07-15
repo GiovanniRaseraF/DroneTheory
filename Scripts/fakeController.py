88fe258f052bf2ae1ff4f4256f860ea2244ad724
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-07-15 14:18:24 +0200

    Creazione della lettura del controller fake

---
+++ Scripts/fakeController.py
@@ -0,0 +1,16 @@
+import vgamepad as vg
+import time
+
+gamepad = vg.VX360Gamepad()
+
+while(True):
+    # press a button to wake the device up
+    gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+    gamepad.update()
+
+    # press buttons and things
+    gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.2)
+    gamepad.right_joystick_float(x_value_float=0, y_value_float=0)
+    gamepad.update()
+
+    time.sleep(0.01)
