fe432cd5647a8ca504ad7c30b02980c1d5ee4ddd
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-07-17 11:54:07 +0200

    Prova dell'utilizzo del controller frsky

--- Scripts/fakeController.py
+++ Scripts/fakeController.py
@@ -4,11 +4,13 @@ import time
 import io
 
 gamepad = vg.VX360Gamepad()
-ser = serial.Serial('COM4', 115200)
+ser = serial.Serial('COM4', 115200, timeout=0)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
 while(True):
     hello = sio.readline()
+    channels = hello.split()
+
     #print(hello)
     # press a button to wake the device up
     #gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
@@ -16,7 +18,11 @@ while(True):
 
     # press buttons and things
     #gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.2)
-    #gamepad.right_joystick_float(x_value_float=0, y_value_float=0)
-    #gamepad.update()
+    if channels:
+        gamepad.right_joystick_float(x_value_float=(float(channels[0]) - 1500)/ 1000, y_value_float=(float(channels[1]) - 1500)/ 1000)
+        gamepad.left_joystick_float(x_value_float=(float(channels[3]) - 1500)/ 1000, y_value_float=(float(channels[2]) - 1500)/ 1000)
+
+
+    gamepad.update()
 
     #time.sleep(0.001)
